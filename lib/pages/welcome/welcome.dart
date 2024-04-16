5c0023cb7ffbfd874147319873122555a0ee58fd
Michela Faella <108745604+MichelaFaella@users.noreply.github.com>
2024-04-16 15:51:25 +0200

    add dots indicator

--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -1,54 +1,92 @@
+import 'package:dots_indicator/dots_indicator.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:mind_mate/pages/utilities/colorsNew.dart';
 import 'package:mind_mate/pages/welcome/welcomeWidgets.dart';
 
-class Welcome extends StatelessWidget {
+class Welcome extends StatefulWidget {
   Welcome({super.key});
 
+  @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
   final PageController _controller = PageController();
+
+  int _dotsIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    return Container(
+      color: colorsNew.backgroundColorNew,
+      child: SafeArea(
+          child: Scaffold(
+            body: Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                //showing the three welcome pages
+                PageView(
+                  onPageChanged: (value){
+                    print("My value is $value");
+                    setState(() {
+                      _dotsIndex = value;
+                    });
+                  },
+                  controller: _controller,
 
-
-    return Scaffold(
-      body: Stack(
-        children: [
-          PageView(
-            controller: _controller,
-
-            scrollDirection: Axis.horizontal,
-            children: [
-              //first page
-              appOnBoardingPage(
-                  _controller,
-                  imagePath: "assets/images/happy.png",
-                  title: "BENVENUTO",
-                  subTitle:
-                      "Io sono Mind, una simpatica lampadina, e ti aiuterò a "
-                          "rimanere concentrato mentre studi. ",
-                  index: 1),
-              //second page
-              appOnBoardingPage(
-                  _controller,
-                  imagePath: "assets/images/run.png",
-                  title: "UNA VASTA SCELTA DI FUNZIONALITA'!",
-                  subTitle:
-                      "Studiando con me potrai prendere appunti, segnare gli "
-                          "obiettivi giorno per giorno e tanto altro ancora!",
-                  index: 2),
-              //third page
-              appOnBoardingPage(
-                  _controller,
-                  imagePath: "assets/images/tongue-out.png",
-                  title: "INIZIAMO",
-                  subTitle:
-                      "Se vuoi il mio efficiente aiuto allora dovremmo "
-                          "conoscerci per bene!",
-                  index: 3),
-            ],
-          ),
-          Positioned(child: Text(""))
-        ],
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    //first page
+                    appOnBoardingPage(
+                        _controller,
+                        imagePath: "assets/images/happy.png",
+                        title: "BENVENUTO",
+                        subTitle:
+                        "Io sono Mind, una simpatica lampadina, e ti aiuterò a "
+                            "rimanere concentrato mentre studi. ",
+                        index: 1),
+                    //second page
+                    appOnBoardingPage(
+                        _controller,
+                        imagePath: "assets/images/run.png",
+                        title: "UNA VASTA SCELTA DI FUNZIONALITA'!",
+                        subTitle:
+                        "Studiando con me potrai prendere appunti, segnare gli "
+                            "obiettivi giorno per giorno e tanto altro ancora!",
+                        index: 2),
+                    //third page
+                    appOnBoardingPage(
+                        _controller,
+                        imagePath: "assets/images/tongue-out.png",
+                        title: "INIZIAMO",
+                        subTitle:
+                        "Se vuoi il mio efficiente aiuto allora dovremmo "
+                            "conoscerci per bene!",
+                        index: 3),
+                  ],
+                ),
+                //showing dots
+                Positioned(
+                    bottom: 30,
+                    child: DotsIndicator(
+                      position: _dotsIndex,
+                      dotsCount: 3,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      decorator: DotsDecorator(
+                          size: const Size.square(9.0),
+                          activeSize: const Size(18.0, 8.0),
+                          activeColor: colorsNew.purpleNew,
+                          activeShape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                          )
+                      ),
+                    ),
+                )
+              ],
+            ),
+          )
       ),
     );
   }
--- lib/pages/welcome/welcomeWidgets.dart
+++ lib/pages/welcome/welcomeWidgets.dart
@@ -1,54 +1,51 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:mind_mate/pages/utilities/colorsNew.dart';
 import 'package:mind_mate/pages/utilities/decorations.dart';
 import 'package:mind_mate/pages/utilities/textWidgets.dart';
 
-Widget appOnBoardingPage(
-    PageController controller,
-      {
-        String imagePath = "",
-        String title = "",
-        String subTitle = "",
-        int index = 0
-      }
-    )
-{
-    return Column(
-      children: [
-        Image.asset(
-          imagePath,
-          fit: BoxFit.fitWidth,
-        ),
-        Container(
-            margin: const EdgeInsets.only(top: 15),
-            decoration: cardDecorations(),
-            width: 319,
-            height: 270,
-            alignment: Alignment.center,
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: text24Normal(text: title),
-                ),
-                Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: text20Normal(text: subTitle))
-              ],
-            )),
-        nextButton(index, controller),
-      ],
-    );
+Widget appOnBoardingPage(PageController controller,
+    {String imagePath = "",
+    String title = "",
+    String subTitle = "",
+    int index = 0}) {
+  return Column(
+    children: [
+      Image.asset(
+        imagePath,
+        fit: BoxFit.fitWidth,
+      ),
+      Container(
+          margin: EdgeInsets.only(top: 15),
+          decoration: cardDecorations(),
+          width: 319,
+          height: 270,
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: text24Normal(text: title),
+              ),
+              Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: text20Normal(text: subTitle))
+            ],
+          )),
+      nextButton(index, controller),
+    ],
+  );
 }
 
 Widget nextButton(int index, PageController controller) {
   return GestureDetector(
     onTap: () {
       print("My index is $index");
-      if(index<3){
+      if (index < 3) {
         controller.animateToPage(
           index,
-          duration: const Duration(milliseconds: 150),
-          curve: Curves.bounceIn,
+          duration: const Duration(milliseconds: 200),
+          curve: Curves.linear,
         );
       }
     },
@@ -59,5 +56,20 @@ Widget nextButton(int index, PageController controller) {
       decoration: buttonDecorations(),
       child: text22Buttons(text: "Avanti"),
     ),
+  );
+}
+
+Widget nextDot(int index) {
+  return DotsIndicator(
+    position: index,
+    dotsCount: 3,
+    mainAxisAlignment: MainAxisAlignment.center,
+    decorator: DotsDecorator(
+        size: const Size.square(9.0),
+        activeSize: const Size(18.0, 8.0),
+        activeColor: colorsNew.purpleNew,
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5.0),
+        )),
   );
 }
